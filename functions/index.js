91a03563a0d0ad15a3d307573c41cf8babb5dff7
Son Phan <sonvphan1@gmail.com>
2020-05-08 23:34:22 -0600

    add user signup route

--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,19 @@ admin.initializeApp(functions.config().firebase);
 const express = require('express');
 const app = express();
 
+var firebaseConfig = {
+    apiKey: 'AIzaSyDtnLGHFwCWsVHlbV8rdUi13DQbqCEH6b0',
+    authDomain: 'chatbee-43d0d.firebaseapp.com',
+    databaseURL: 'https://chatbee-43d0d.firebaseio.com',
+    projectId: 'chatbee-43d0d',
+    storageBucket: 'chatbee-43d0d.appspot.com',
+    messagingSenderId: '849862413464',
+    appId: '1:849862413464:web:cfb0ec795ce7a20de794ed',
+};
+
+const firebase = require('firebase');
+firebase.initializeApp(firebaseConfig);
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -48,6 +61,33 @@ app.post('/buzz', (req, res) => {
         .catch((err) => {
             res.status(500).json({ error: 'Something went wrong' });
             console.error(err);
+        });
+});
+
+// Signup route
+app.post('/signup', (req, res) => {
+    const newUser = {
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        handle: req.body.handle,
+    };
+
+    // TODO: validate data
+
+    firebase
+        .auth()
+        .createUserWithEmailAndPassword(newUser.email, newUser.password)
+        .then((data) => {
+            return res
+                .status(201)
+                .json({
+                    message: `User ${data.user.id} signed up successfully!`,
+                });
+        })
+        .catch((error) => {
+            console.error(error);
+            return res.status(500).json({ error: err.code });
         });
 });
 
